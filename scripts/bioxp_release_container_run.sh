502d97c6b2d4b24ddcf6145d6caf576a5d5ee759
MolBioFreak <teaguechristian@gmail.com>
2026-08-27 23:06:15 -0500

    fix udocker repository option ordering

--- scripts/bioxp_release_container_run.sh
+++ scripts/bioxp_release_container_run.sh
@@ -399,8 +399,7 @@ SOURCE_VOLUME=()
 [[ "$SOURCE_MODE" == exact_commit_materialization ]] || fail "unsupported source mode"
 SOURCE_VOLUME=(--volume="$HOST_SOURCE:/app:ro")
 
-exec "$UDOCKER_BIN" run \
-  --repo="$UDOCKER_ROOT/store" \
+exec "$UDOCKER_BIN" --repo="$UDOCKER_ROOT/store" run \
   --pull=never \
   --user=root \
   --env="BIOXP_RELEASE_ID=$RELEASE_ID" \
